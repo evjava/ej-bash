595a5f0b638d7f19080651557e78454af9964f8b
evjava <evjava@yandex.ru>
2024-03-13 13:42:42 +0300

    master: ej-setup-emacs fixes

--- ej-setup-emacs.sh
+++ ej-setup-emacs.sh
@@ -2,38 +2,44 @@
 emacs_dir='emacs-29.2'
 
 cd /tmp
-wget -c https://ftp.gnu.org/gnu/emacs/$emacs_dir.tar.gz
-tar -xvf $emacs_dir.tar.gz
+if ! test -f $emacs_dir.tar.gz; then
+    wget https://ftp.gnu.org/gnu/emacs/$emacs_dir.tar.gz
+fi
+if ! test -d $emacs_dir; then
+    tar -xvf $emacs_dir.tar.gz
+fi
 cd $emacs_dir
 
-CC="gcc-10" ./autogen.sh
-sai texinfo \
-    xaw3dg-dev \
-    gcc-10 \
-    build-essential \
-    libgccjit-10* \
-    acl \
-    libacl1* \
-    xaw3dg* \
-    xaw3dg-dev \
-    libmagickwand-dev \
-    libm17n-dev \
-    libmagickcore-dev \
-    libgtk-3-dev \
-    libwebkit2gtk-4.0-dev \
-#    libghc-gnutls-dev \
-    libgnutls30 \
-    libwebkit2gtk-4.1-dev \
-    giflib-tools \
-    libgif7 \
-    libgif-dev \
-    libgnutls28-dev \
-    libtree-sitter-dev \
-    libjansson4 \
-    libjansson-dev \
-    imagemagick-6.q16hdri \
-    dvipng
+sudo apt install -y \
+     make \
+     texinfo \
+     xaw3dg-dev \
+     gcc-10 \
+     build-essential \
+     libgccjit-10* \
+     acl \
+     libacl1* \
+     xaw3dg* \
+     xaw3dg-dev \
+     libmagickwand-dev \
+     libm17n-dev \
+     libmagickcore-dev \
+     libgtk-3-dev \
+     libwebkit2gtk-4.0-dev \
+     libncurses-dev \
+     libgnutls30 \
+     libwebkit2gtk-4.1-dev \
+     giflib-tools \
+     libgif \
+     libgif-dev \
+     libgnutls28-dev \
+     libtree-sitter-dev \
+     libjansson4 \
+     libjansson-dev \
+     imagemagick-6.q16hdri \
+     dvipng
 
+CC='gcc-10" ./autogen.sh
 # mmap not supported yet...
 CC="gcc-10" ./configure  --with-mailutils --with-imagemagick --with-xwidgets --with-native-compilation --with-libsystemd --with-x-toolkit=gtk3 --with-gconf --with-gpm --with-m17n-flt --with-libotf --with-xft
 
