8f6f8050a1a7bde47e11aad11eba55a7bab4ce0f
Eugene Tagin <tagin@airi.net>
2025-07-25 14:55:54 +0300

    dev: fixed git setup

--- ej-setup-env.sh
+++ ej-setup-env.sh
@@ -72,12 +72,13 @@ function update-xdg () {
 
 function setup-git () {
     echo 'Updating ~/.gitconfig...'
-    git_conf () { git config --global $1 $2; }
+    git_conf () { git config --global $1 "$2"; }
     git_conf core.editor "emacsclient"
     git_conf alias.st "status"
     git_conf alias.ci "commit"
     git_conf alias.co "checkout"
     git_conf alias.br "branch"
+    git_conf credential.helper "cache --timeout=36000"
     # git_conf alias.hist 'log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
 }
 
